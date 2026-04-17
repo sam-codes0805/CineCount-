b504dd6c384b9089ea07be2d59507197e303613a
Somesh Kumar <someshkumar0805@gmail.com>
2026-04-17 06:38:09 +0530

    fixed some bugs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -87,8 +87,8 @@ const Home = (props) => {
           />
           
           {/* Cinematic Overlay (Darkens bottom and left for text readability) */}
-          <div className="absolute inset-0 bg-gradient-to-t from-black via-black/40 to-transparent" />
-          <div className="absolute inset-0 bg-gradient-to-r from-black/60 via-transparent to-transparent" />
+          <div className="absolute inset-0 bg-linear-to-t from-black via-black/40 to-transparent" />
+          <div className="absolute inset-0 bg-linear-to-r from-black/60 via-transparent to-transparent" />
 
           {/* Text Content */}
           <div className="absolute bottom-12 left-8 md:left-16 max-w-2xl animate-in fade-in slide-in-from-left-8 duration-700">
